07a133b6cb506a91cb0c75deccce05a851bbf0f3
David-Jr <david0gjunior@gmail.com>
2023-04-09 08:45:26 +0200

    Melhorei a estrutura do code em RouteDistanceTime.java.

--- app/src/main/java/com/groupjob/mapapp/RouteDistanceTime.java
+++ app/src/main/java/com/groupjob/mapapp/RouteDistanceTime.java
@@ -13,6 +13,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -28,94 +29,43 @@ public class RouteDistanceTime {
     private static final String API_KEY = "AIzaSyDQDEPMcXgETn-yN__zU2gSYHfTGfk9pr4";
     private static final String API_URL = "https://routes.googleapis.com/directions/v2:computeRoutes";
 
-    /*
-    public static void main(String[] args) throws Exception {
-        URL url = new URL("https://routes.googleapis.com/directions/v2:computeRoutes");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("POST");
 
-        // Set request headers
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("X-Goog-Api-Key", "AIzaSyDQDEPMcXgETn-yN__zU2gSYHfTGfk9pr4");
-        con.setRequestProperty("X-Goog-FieldMask", "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline");
-
-        // Construct request body as a JSON object
-        JSONObject requestBody = new JSONObject();
-        requestBody.put("origin", new JSONObject()
-                .put("location", new JSONObject()
-                        .put("latLng", new JSONObject()
-                                .put("latitude", 37.419734)
-                                .put("longitude", -122.0827784)
-                        )
-                )
-        );
-        requestBody.put("destination", new JSONObject()
-                .put("location", new JSONObject()
-                        .put("latLng", new JSONObject()
-                                .put("latitude", 37.417670)
-                                .put("longitude", -122.079595)
-                        )
-                )
-        );
-        requestBody.put("travelMode", "DRIVE");
-        requestBody.put("routingPreference", "TRAFFIC_AWARE");
-        requestBody.put("departureTime", "2023-04-08T15:30:45.1234560Z");
-//        requestBody.put("departureTime", "2023-10-15T15:01:23.045123456Z"); // Original
-        requestBody.put("computeAlternativeRoutes", false);
-        requestBody.put("routeModifiers", new JSONObject()
-                .put("avoidTolls", false)
-                .put("avoidHighways", false)
-                .put("avoidFerries", false)
-        );
-        requestBody.put("languageCode", "en-US");
-        requestBody.put("units", "IMPERIAL");
-
-        // Send the request
-        con.setDoOutput(true);
-        byte[] requestBodyBytes = requestBody.toString().getBytes("utf-8");
-        con.setRequestProperty("Content-Length", Integer.toString(requestBodyBytes.length));
-        con.getOutputStream().write(requestBodyBytes);
-
-        // Read the response
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        // Print the response as a string
-        System.out.println(response.toString());
-
-        // Parse the response as a JSON object
-        JSONObject responseBody = new JSONObject(response.toString());
-        // Do something with the response body
-    }
-*/
-
-    public static String getDistance(LatLng location, LatLng destination) throws JSONException {
-        JSONObject responseBody = new JSONObject();
+    /**
+     *
+     * @param location - A localizacao atual do utilizador
+     * @param destination - O destino selecionado pelo utilizador
+     * @return String[]{distanceMeters, durationSeconds}
+     */
+    public static String[] getInformationArray(LatLng location, LatLng destination) {
+//        String[] informationArray = new String[2];
         try {
             HttpURLConnection connection = buildConnection();
             JSONObject requestBody = buildRequestBody(location, destination);
-            responseBody = sendRequest(connection, requestBody);
-            return String.valueOf(responseBody.getJSONArray("routes")
-                    .getJSONObject(0)
-                    .getInt("distanceMeters"));
+            JSONObject responseBody = sendRequest(connection, requestBody);
+            Log.d("DEBUGGING", "==========================================!");
+            return new String[]{
+                    String.valueOf(responseBody.getJSONArray("routes")
+                            .getJSONObject(0)
+                            .getInt("distanceMeters")
+                    ),
+                    responseBody.getJSONArray("routes")
+                            .getJSONObject(0)
+                            .getString("duration")
+            };
         } catch (IOException e) {
             Log.d("EXCEPTION-LOG", "Conexao mal formada!");
+            e.printStackTrace();
+            return new String[]{};
+
         } catch (JSONException e) {
             Log.d("EXCEPTION-LOG", "RequestBody mal formado!");
+            e.printStackTrace();
+            return new String[]{};
         }
-        return String.valueOf(
-                responseBody.getJSONArray("routes")
-                        .getJSONObject(0)
-                        .getInt("distanceMeters"));
     }
 
     @NonNull
-    public static JSONObject buildRequestBody(LatLng currentLocation, LatLng destination) throws JSONException {
+    private static JSONObject buildRequestBody(LatLng currentLocation, LatLng destination) throws JSONException {
         // Construct request body as a JSON object
         JSONObject requestBody = new JSONObject();
         requestBody.put("origin", new JSONObject()
@@ -150,7 +100,7 @@ public class RouteDistanceTime {
     }
 
     @NonNull
-    public static HttpURLConnection buildConnection() throws IOException {
+    private static HttpURLConnection buildConnection() throws IOException {
         URL url = new URL(API_URL);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("POST");
@@ -163,9 +113,14 @@ public class RouteDistanceTime {
     }
 
     private static JSONObject sendRequest(@NonNull HttpURLConnection con, @NonNull JSONObject requestBody) throws IOException, JSONException {
+
+        // Send request
         con.setDoOutput(true);
-        byte[] requestBodyBytes = requestBody.toString().getBytes("utf-8");
+
+        Log.d("EXCEPTION-LOG", requestBody.toString());
+        byte[] requestBodyBytes = requestBody.toString().getBytes(StandardCharsets.UTF_8);
         con.setRequestProperty("Content-Length", Integer.toString(requestBodyBytes.length));
+
         con.getOutputStream().write(requestBodyBytes);
 
         // Read the response
@@ -176,10 +131,6 @@ public class RouteDistanceTime {
             response.append(inputLine);
         }
         in.close();
-
-        // Print the response as a string
-//        System.out.println(response.toString());
-
         return  new JSONObject(response.toString());
     }
 
